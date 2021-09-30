36ca27f29b757169a1b163ee4200ad4548c3d528
buzzracharyeah rejan <rejandev@gmail.com>
2021-09-30 11:19:45 +0545

    useState - array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('bananas');
+  // console.log(useState('say hello'));
+  // const value = useState(1)[0]; 
+  // const handler = useState(1)[1]; 
+  // console.log(value, handler); 
+  const clickHandler = () => {
+    if(text === 'bananas') {
+      setText('apples');
+    } else {
+      setText('bananas');
+    }
+  }
+  
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type='button' className='btn' onClick={clickHandler}>change title</button>
+    </React.Fragment>
+  );
 };
+
 
 export default UseStateBasics;
