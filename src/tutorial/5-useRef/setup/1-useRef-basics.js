bfcb5d5904e4aa3d182e76811ad7f7b9d62084e9
buzzracharyeah rejan <rejandev@gmail.com>
2021-10-04 22:20:35 +0545

    useReducer hook

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -3,9 +3,27 @@ import React, { useEffect, useRef } from 'react';
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
-
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null); 
+
+  const submitHandler = e => {
+    e.preventDefault(); 
+    console.log(refContainer.current.value);
+  }
+
+  useEffect(() => {
+    refContainer.current.focus(); 
+    refContainer.current.value = '';
+  })
+  console.log(refContainer);
+  return (
+    <form className='form' onSubmit={submitHandler}>
+      <div>
+        <input type="text" ref={refContainer}/>
+        <button className="btn">submit</button>
+      </div>
+    </form>
+  )
 };
 
 export default UseRefBasics;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,55 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
-// reducer function
-
+import React, {useState, useReducer} from 'react'; 
+import Modal from './Modal'
+const defState = {
+  people: [], 
+  isModal: false, 
+  modalContent: 'say hi'
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  // const [showModal, setShowModal] = useState(false); 
+  // const [modalContent, setModalContent] = useState('say hello');
+  
+const reducer = (state, action) => {
+  // console.log(state);
+  switch(action.type) {
+    case 'add': 
+    return {...state, people: [...state.people,
+      {id: new Date().getTime().toString(), name},
+    ], 
+    isModal: true, 
+    modalContent: 'person added', 
+  }
+    default: 
+    throw new Error('invalid choice');
 
-export default Index;
+  }
+
+}
+
+  const [name,setName] = useState(''); 
+  const [state, dispatch] = useReducer(reducer, defState); 
+
+
+  const submitHandler = e => {
+    e.preventDefault(); 
+    dispatch({type: 'add'})
+  }
+  return (
+    <React.Fragment>
+      {state.isModal && <Modal modalContent={state.modalContent}/>}
+      <form className='form' onSubmit={submitHandler}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id='name' value={name}
+        onChange={e => setName(e.target.value)} />
+        <button>submit</button> 
+      </form>
+      {state.people.map(person => (
+        <div key={person.id}>
+          <h4>{person.name}</h4>
+        </div>
+      ))}
+    </React.Fragment>
+  )
+}
+
+export default Index; 
