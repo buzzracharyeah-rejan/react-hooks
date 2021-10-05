277f0055b96d55b6164a106869bd883d952daf97
buzzracharyeah rejan <rejandev@gmail.com>
2021-10-05 16:48:51 +0545

    custom hooks and prop types

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types'; 
+import defaultImg from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({name, image, price}) => {
+  return <article className='product'>
+    <img src={image.url || defaultImg} alt={name} />
+    <h4>{name}</h4>
+    <p>${price}</p>
+
+  </article>;
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired, 
+  name: PropTypes.string.isRequired, 
+  price: PropTypes.number.isRequired, 
+}; 
+
+Product.defaultProps = {
+  name: 'default name', 
+  price: 3.99, 
+  image: defaultImg
+}
 
 export default Product;
